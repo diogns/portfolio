28170734e1c1f10118de8410b189651fa13e9aa7
rmorales <raul.morales@entel.pe>
2023-03-16 09:58:45 -0500

    build: :label: Init project setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen text-center'>
+            <div className='max-w'>
+
+            </div>
+            <h1>Main</h1>
+        </div>
+    )
+}
+
+export default Main;
